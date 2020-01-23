31e842ca2fcbe3ba005168a26483d4a1fda86d17
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-01-23 07:35:54 +0100

    Finished class

--- src/com/example/collections/Theatre.java
+++ src/com/example/collections/Theatre.java
@@ -1,6 +1,6 @@
 package com.example.collections;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Theatre {
 
@@ -23,5 +23,60 @@ public class Theatre {
         return theatreName;
     }
 
+    public boolean reserveSeat(String seatNumber) {
+        Seat requestedSeat = null;
+        for (Seat seat: seats) {
+            if (seat.getSeatNumber().equals(seatNumber)) {
+                requestedSeat = seat;
+                break;
+            }
+        }
 
+        if (requestedSeat == null) {
+            System.out.println("There is no seat " + seatNumber);
+            return false;
+        }
+
+        return requestedSeat.reserve();
+    }
+
+    public void getSeats() {
+        for (Seat seat : seats) {
+            System.out.println(seat.getSeatNumber());
+        }
+    }
+
+    private class Seat {
+        private final String seatNumber;
+        private boolean reserved = false;
+
+        public Seat(String seatNumber) {
+            this.seatNumber = seatNumber;
+        }
+
+        public boolean reserve() {
+            if (!this.reserved) {
+                this.reserved = true;
+                System.out.println("Seat " + seatNumber + " reserved");
+                return true;
+            } else {
+
+                return false;
+            }
+        }
+
+        public boolean cancel() {
+            if (this.reserved) {
+                this.reserved = false;
+                System.out.println("Reservation of seat" + seatNumber + " cancelled");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public String getSeatNumber() {
+            return seatNumber;
+        }
+    }
 }
