c431ed415dcf217daf0e1c2f40e9445314dfaec1
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-01-27 21:56:32 +0100

    Testing shuffle and min-/max-values

--- src/com/example/collections/Collections.java
+++ src/com/example/collections/Collections.java
@@ -1,19 +1,41 @@
 package com.example.collections;
 
+import javax.swing.text.html.ImageView;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Collections {
 
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olympian", 8, 12);
-        //theatre.getSeats();
-        if (theatre.reserveSeat("B13")) {
-            System.out.println("Please pay");
+        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+        printList(seatCopy);
+
+        seatCopy.get(1).reserve();
+        if (theatre.reserveSeat("A02")) {
+            System.out.println("Please pay for A02");
         } else {
-            System.out.println("Sorry, seat is taken");
+            System.out.println("Seat already reserved");
         }
-        /*if (theatre.reserveSeat("H11")) {
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry, seat is taken");
-        }*/
+
+        java.util.Collections.shuffle(seatCopy);
+        System.out.println("Printing seatCopy");
+        printList(seatCopy);
+        System.out.println("Printing theatre.seat");
+        printList(theatre.seats);
+
+        Theatre.Seat minSeat = java.util.Collections.min(seatCopy);
+        Theatre.Seat maxSeat = java.util.Collections.max(seatCopy);
+        System.out.println("Min seat number is " + minSeat.getSeatNumber() + ", max seat number is "+ maxSeat.getSeatNumber());
+    }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for (Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+
+        System.out.println();
+        System.out.println("==============================================================================");
     }
 }
