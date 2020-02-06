274d044499b0be367f62c4a4e28a15aac3485dca
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-02-06 15:08:55 +0100

    Insert values to Hashmap

--- src/com/example/collections/MapProgram.java
+++ src/com/example/collections/MapProgram.java
@@ -7,6 +7,12 @@ public class MapProgram {
 
     public static void main(String[] args) {
         Map<String, String> languages = new HashMap<>();
-        languages.put("java", "a compiled high level, object-oriented, platform independent language");
+        languages.put("Java", "A compiled high level, object-oriented, platform independent language");
+        languages.put("Python", "An interpereted, object-oriented, high-level programming language with dynamic semantics");
+        languages.put("Algol", "An algothmic language");
+        languages.put("BASIC", "Beginners all purpose symbolic instruction code");
+        languages.put("Lisp", "Therin lies madness");
+
+        System.out.println(languages.get("Java"));
     }
 }
