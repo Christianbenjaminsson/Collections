b45f4a0739f790199e374331e8da540b61d126e8
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-02-10 20:19:48 +0100

    Print values from Hashmap using languages.get(key)

--- src/com/example/collections/MapProgram.java
+++ src/com/example/collections/MapProgram.java
@@ -7,12 +7,31 @@ public class MapProgram {
 
     public static void main(String[] args) {
         Map<String, String> languages = new HashMap<>();
-        languages.put("Java", "A compiled high level, object-oriented, platform independent language");
+        if (languages.containsKey("Java")) {
+            System.out.println("Java already exists");
+        } else {
+            languages.put("Java", "A compiled high level, object-oriented, platform independent language");
+            System.out.println("Java added successfully");
+        }
         languages.put("Python", "An interpereted, object-oriented, high-level programming language with dynamic semantics");
         languages.put("Algol", "An algothmic language");
         languages.put("BASIC", "Beginners all purpose symbolic instruction code");
         languages.put("Lisp", "Therin lies madness");
 
+        if (languages.containsKey("Java")) {
+            System.out.println("Java is already in the map");
+        } else {
+            languages.put("Java","this course is about Java");
+        }
         System.out.println(languages.get("Java"));
+        languages.put("Java", "this course is about Java");
+        System.out.println(languages.get("Java"));
+
+        System.out.println();
+        System.out.println("==========================================================================");
+
+        for (String key: languages.keySet()) {
+            System.out.println(key + " : " + languages.get(key));
+        }
     }
 }
